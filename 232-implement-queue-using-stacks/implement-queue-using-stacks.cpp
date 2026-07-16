df47f2b0137d7c09ab071fd8543e158d7cf1f6ee
manasvisangwan <manasvisangwan@gmail.com>
2026-07-16 12:49:50 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (6.68%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -0,0 +1,40 @@
+class MyQueue {
+private:
+    stack<int> in;
+    stack<int> out;
+
+    void transfer() {
+        while (!in.empty()) {
+            out.push(in.top());
+            in.pop();
+        }
+    }
+
+public:
+    MyQueue() {
+    }
+
+    void push(int x) {
+        in.push(x);
+    }
+
+    int pop() {
+        if (out.empty())
+            transfer();
+
+        int val = out.top();
+        out.pop();
+        return val;
+    }
+
+    int peek() {
+        if (out.empty())
+            transfer();
+
+        return out.top();
+    }
+
+    bool empty() {
+        return in.empty() && out.empty();
+    }
+};
